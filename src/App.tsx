4d2d8910b241e7ded1344683f41329a0d78e3589
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-04-10 16:15:58 +0530

    slider added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Sliding from "./components/sliding";
+
+const App = () => {
+  return (
+    <>
+      <Box mt={2} mb={2} p={2} display="flex" gap={5} bgcolor="black">
+        <Box height="50vh" width="100%" overflow="hidden" padding={2}>
+          <Sliding />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/sliding.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Slider from "react-slick";
+import { Box } from "@mui/material";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Sliding = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    vertical: false, // Set vertical to false to display images from left to right
+  };
+
+  return (
+    <Box maxWidth="600px" mx="auto">
+      <Slider {...settings}>
+        <Box>
+          <img
+            src="src/assets/images/image_1.jpg"
+            alt="Image 1"
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <img
+            src="src/assets/images/image_2.jpg"
+            alt="Image 2"
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <img
+            src="src/assets/images/image_3.jpg"
+            alt="Image 3"
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <img
+            src="src/assets/images/image_4.jpg"
+            alt="Image 4"
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <img
+            src="src/assets/images/image_5.jpg"
+            alt="Image 5"
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <img
+            src="src/assets/images/image_6.jpg"
+            alt="Image 6"
+            style={{ width: "100%" }}
+          />
+        </Box>
+      </Slider>
+    </Box>
+  );
+};
+
+export default Sliding;
