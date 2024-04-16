82ddf0e046a23324b8a4083b1d80c8ed33837f95
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-04-16 12:25:45 +0530

    removed set timeout in notistack

--- src/components/notistack/notistack.tsx
+++ src/components/notistack/notistack.tsx
@@ -5,17 +5,15 @@ import { Button, Box } from "@mui/material";
 type Props = {};
 
 const Notistack = (props: Props) => {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
+
   return (
     <Box bgcolor="white" p={1}>
       <Button
         onClick={() => {
-          const snackbarKey = enqueueSnackbar("I like hooks", {
-            autoHideDuration: 5000,
+          enqueueSnackbar("I like hooks", {
+            autoHideDuration: 3000,
           });
-          setTimeout(() => {
-            closeSnackbar(snackbarKey);
-          }, 8000); // Automatically close after 5 seconds
         }}
       >
         Show snackbar
